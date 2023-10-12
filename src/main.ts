7a8af14dfbf06f2851188fac0c4610480212c0d6
dongyuekai <457545648@qq.com>
2023-10-12 17:46:42 +0800

    feat: 请求http的库可以在express和fastify之间切换

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule); // 默认是express的适配器
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  ); // 切换到fastify平台
+  app.useStaticAssets;
   await app.listen(3000);
 }
 bootstrap();
